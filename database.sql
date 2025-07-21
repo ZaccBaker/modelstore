78ba1e99c4002bfdccfb7bedc12f68188bbc08f5
ZaccBaker <zac.baker232@gmail.com>
2025-07-20 19:13:51 -0500

    Initial database complete upload

--- database.sql
+++ database.sql
@@ -49,7 +49,7 @@ CREATE TABLE inventory(
 CREATE TABLE customer(
     customer_id                 INT             NOT NULL        AUTO_INCREMENT,
     customer_firstName          VARCHAR(75)     NOT NULL,
-    customer_LastName           VARCHAR(75)     NOT NULL,
+    customer_lastName           VARCHAR(75)     NOT NULL,
     customer_phone              VARCHAR(25),
     customer_email              VARCHAR(50),
     category_id                 INT             NOT NULL,
@@ -147,69 +147,6 @@ INSERT INTO vendor(vendor_name, vendor_email)
 INSERT INTO vendor(vendor_name, vendor_phone)
     VALUES('Merch World', '891.255.4776');
 
--- Insert discount
-INSERT INTO discount(discount_percent)
-    VALUES(0);
-
-INSERT INTO discount(discount_percent)
-    VALUES(15);
-
-INSERT INTO discount(discount_percent)
-    VALUES(20);
-
--- Insert category: types and connect with discount
-INSERT INTO category(category_type, discount_id)
-    VALUES('Student', 1);
-
-INSERT INTO category(category_type, discount_id)
-    VALUES('Parent', 1);
-
-INSERT INTO category(category_type, discount_id)
-    VALUES('Teacher', 2);
-
-INSERT INTO category(category_type, discount_id)
-    VALUES('Faculty Member', 3);
-
--- Insert customer
-
-
-
--- Insert payment 
-INSERT INTO payment(payment_type)
-    VALUES('Cash');
-
-INSERT INTO payment(payment_type)
-    VALUES('Card');
-
-INSERT INTO payment(payment_type)
-    VALUES('Touchless');
-
--- Insert clearance
-INSERT INTO clearance(clearance_type)
-    VALUES('Manager');
-
-INSERT INTO clearance(clearance_type)
-    VALUES('Assistant Manager');
-
-INSERT INTO clearance(clearance_type)
-    VALUES('General');
-
--- Insert employee
-INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
-    VALUES('Lucy', 'Bryan', '531.115.8843', 'lucybryan@school.org', 1);
-
-INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
-    VALUES('Gloria', 'Orozco', '473.156.3218', 'gloriaorozco@school.org', 2);
-
-INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
-    VALUES('Natalie', 'Larson', '469.863.6647', 'natalielarson@school.org', 3);
-
-INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
-    VALUES('Elizabeth', 'Callahan', '732.947.3344', 'elizabethcallahan@school.org', 3);
-
-INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
-    VALUES('Edna', 'Mann', '399.654.4468', 'ednamann@school.org', 3);
-
 -- Insert inventory
 INSERT INTO inventory(inventory_name, inventory_quantity, inventory_cost, inventory_retail, vendor_id)
     VALUES('Shirt', '52', 7.50, 9.25, 3);
@@ -229,4 +166,106 @@ INSERT INTO inventory(inventory_name, inventory_quantity, inventory_cost, invent
 INSERT INTO inventory(inventory_name, inventory_quantity, inventory_cost, inventory_retail, vendor_id)
     VALUES('Hat', '33', 9.75, 15.99, 1);
 
--- Insert 
+-- Insert customer
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Rowan', 'Aguirre', '135.554.4138', 'rowanaguirre@school.org', 3);
+
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Ann', 'Irwin', '318.551.6876', 'annirwin@school.org', 2);
+
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Luther', 'Mendez', '486.135.8432', 'luthermendez@school.org', 3);
+
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Jaden', 'Ali', '948.165.3218', 'jadenali@school.org', 1);
+
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Archie', 'Chambers', '123.843.6135', 'archiechambers@school.org', 1);
+
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Osman', 'Hardy', '012.154.7355', 'osmanhardy@school.org', 4);
+
+INSERT INTO customer(customer_firstName, customer_lastName, customer_phone, customer_email, category_id)
+    VALUES('Isabel', 'Atkins', '789.515.6548', 'isabelatkins@school.org', 1);
+
+-- Insert category: types and connect with discount
+INSERT INTO category(category_type, discount_id)
+    VALUES('Student', 1);
+
+INSERT INTO category(category_type, discount_id)
+    VALUES('Parent', 1);
+
+INSERT INTO category(category_type, discount_id)
+    VALUES('Teacher', 2);
+
+INSERT INTO category(category_type, discount_id)
+    VALUES('Faculty Member', 3);
+
+-- Insert discount
+INSERT INTO discount(discount_percent)
+    VALUES(0);
+
+INSERT INTO discount(discount_percent)
+    VALUES(15);
+
+INSERT INTO discount(discount_percent)
+    VALUES(20);
+
+-- Insert sales - placeholder totalPrice
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(79.88, 1, 3, 2);
+
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(50.22, 5, 1, 2);
+
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(20.15, 2, 5, 3);
+
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(120.99, 4, 2, 2);
+
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(55.00, 1, 4, 2);
+
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(25.15, 2, 6, 1); 
+
+INSERT INTO sales(sales_totalPrice, inventory_id, customer_id, payment_id)
+    VALUES(36.75, 3, 3, 2);
+
+-- Insert payment 
+INSERT INTO payment(payment_type)
+    VALUES('Cash');
+
+INSERT INTO payment(payment_type)
+    VALUES('Card');
+
+INSERT INTO payment(payment_type)
+    VALUES('Touchless');
+
+
+-- Insert employee
+INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
+    VALUES('Lucy', 'Bryan', '531.115.8843', 'lucybryan@school.org', 1);
+
+INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
+    VALUES('Gloria', 'Orozco', '473.156.3218', 'gloriaorozco@school.org', 2);
+
+INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
+    VALUES('Natalie', 'Larson', '469.863.6647', 'natalielarson@school.org', 3);
+
+INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
+    VALUES('Elizabeth', 'Callahan', '732.947.3344', 'elizabethcallahan@school.org', 3);
+
+INSERT INTO employee(employee_firstName, employee_lastName, employee_phone, employee_email, clearance_id)
+    VALUES('Edna', 'Mann', '399.654.4468', 'ednamann@school.org', 3);
+
+-- Insert clearance
+INSERT INTO clearance(clearance_type)
+    VALUES('Manager');
+
+INSERT INTO clearance(clearance_type)
+    VALUES('Assistant Manager');
+
+INSERT INTO clearance(clearance_type)
+    VALUES('General');
